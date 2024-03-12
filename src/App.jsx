75fa39c8b54adac53f89fb60c4bb6f0db9e3c4ca
shakurShirajul <siirajulisllam@gmail.com>
2024-03-12 19:53:52 +0600

    JSON Data Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,16 @@
 // import { useState } from 'react'
 
 import './App.css'
+import Header from './components/header'
+import Blogs from './components/blogs'
 
 function App() {
   // const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <Header/>
+      <Blogs></Blogs>
     </>
   )
 }
---
+++ src/components/blogs.jsx
@@ -0,0 +1,19 @@
+import { useEffect } from "react";
+import { useState } from "react";
+
+
+const Blogs = () => {
+    const [blogs, setblogs] = useState([]);
+    useEffect(()=>{
+        fetch('blogs.json')
+        .then(res=>res.json())
+        .then(data=>setblogs(data))
+    },[])
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default Blogs;
